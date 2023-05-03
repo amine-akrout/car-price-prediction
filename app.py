97fe9c07927a144a22cd90bdb2996c0696309408
Amine AKROUT <akrout.med.amine@gmail.com>
2023-05-03 17:27:14 +0200

    add pyproject.toml

--- app.py
+++ app.py
@@ -1,67 +1,122 @@
-# Import Libraries
+"""
+This is a simple Flask web app that predicts the price of a car based on the user inputs.
+"""
 from __future__ import print_function
+import os
 import sys
 import mlflow
 from flask import Flask, render_template, request
 import pandas as pd
 
 
-logged_model = './model'
+MODEL_PATH = "./model"
 # Load model as a PyFuncModel.
-loaded_model = mlflow.pyfunc.load_model(logged_model)
+LOADED_MODEL = mlflow.pyfunc.load_model(MODEL_PATH)
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def entry_page():
-    # Nicepage template of the webpage
-    return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/")
+def entry_page():
+    """Displays the input webpage"""
+    # Nicepage template of the webpage
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["GET", "POST"])
 def render_message():
+    """Generates the predicted price and renders it in the html"""
     try:
         # Get data input
-        CarBrand = request.form['CarBrand']
-        fueltype = request.form['fueltype']
-        aspiration = request.form['aspiration']
-        doornumber = request.form['doornumber']
-        carbody = request.form['carbody']
-        drivewheel = request.form['drivewheel']
-        enginelocation = request.form['enginelocation']
-        wheelbase = float(request.form['wheelbase'])
-        carlength = float(request.form['carlength'])
-        carwidth = float(request.form['carwidth'])
-        carheight = float(request.form['carheight'])
-        curbweight = int(request.form['curbweight'])
-        enginetype = request.form['enginetype']
-        cylindernumber = request.form['cylindernumber']
-        enginesize = int(request.form['enginesize'])
-        fuelsystem = request.form['fuelsystem']
-        boreratio = float(request.form['boreratio'])
-        horsepower = int(request.form['horsepower'])
-        citympg = int(request.form['citympg'])
-        highwaympg = int(request.form['highwaympg'])
-        data = [[CarBrand, fueltype, aspiration, doornumber, carbody, drivewheel, enginelocation,  wheelbase, carlength,
-        carwidth, carheight, curbweight, enginetype, cylindernumber, enginesize, fuelsystem,  boreratio, horsepower,
-        citympg, highwaympg]]
-        df = pd.DataFrame(data, columns = ['CarBrand', 'fueltype', 'aspiration', 'doornumber', 'carbody', 'drivewheel',
-                            'enginelocation', 'wheelbase', 'carlength', 'carwidth', 'carheight','curbweight', 'enginetype',
-                            'cylindernumber', 'enginesize','fuelsystem', 'boreratio', 'horsepower', 'citympg', 'highwaympg'])
+        CarBrand = request.form["CarBrand"]
+        fueltype = request.form["fueltype"]
+        aspiration = request.form["aspiration"]
+        doornumber = request.form["doornumber"]
+        carbody = request.form["carbody"]
+        drivewheel = request.form["drivewheel"]
+        enginelocation = request.form["enginelocation"]
+        wheelbase = float(request.form["wheelbase"])
+        carlength = float(request.form["carlength"])
+        carwidth = float(request.form["carwidth"])
+        carheight = float(request.form["carheight"])
+        curbweight = int(request.form["curbweight"])
+        enginetype = request.form["enginetype"]
+        cylindernumber = request.form["cylindernumber"]
+        enginesize = int(request.form["enginesize"])
+        fuelsystem = request.form["fuelsystem"]
+        boreratio = float(request.form["boreratio"])
+        horsepower = int(request.form["horsepower"])
+        citympg = int(request.form["citympg"])
+        highwaympg = int(request.form["highwaympg"])
+        data = [
+            [
+                CarBrand,
+                fueltype,
+                aspiration,
+                doornumber,
+                carbody,
+                drivewheel,
+                enginelocation,
+                wheelbase,
+                carlength,
+                carwidth,
+                carheight,
+                curbweight,
+                enginetype,
+                cylindernumber,
+                enginesize,
+                fuelsystem,
+                boreratio,
+                horsepower,
+                citympg,
+                highwaympg,
+            ]
+        ]
+        preds_df = pd.DataFrame(
+            data,
+            columns=[
+                "CarBrand",
+                "fueltype",
+                "aspiration",
+                "doornumber",
+                "carbody",
+                "drivewheel",
+                "enginelocation",
+                "wheelbase",
+                "carlength",
+                "carwidth",
+                "carheight",
+                "curbweight",
+                "enginetype",
+                "cylindernumber",
+                "enginesize",
+                "fuelsystem",
+                "boreratio",
+                "horsepower",
+                "citympg",
+                "highwaympg",
+            ],
+        )
 
-        preds = loaded_model.predict(pd.DataFrame(df))
+        preds = LOADED_MODEL.predict(pd.DataFrame(preds_df))
 
-        print('Python module executed successfully')
-        message = 'Estimated price : {} '.format(round(preds[0],2))
+        print("Python module executed successfully")
+        message = "Estimated price : {} ".format(round(preds[0], 2))
         print(message, file=sys.stderr)
 
-    except Exception as e:
+    except Exception as error:
         # Store error to pass to the web page
-        message = "Error encountered. Try with other values. ErrorClass: {}, Argument: {} and Traceback details are: {}".format(
-            e.__class__, e.args, e.__doc__)
+        message = (
+            "Error encountered. Try with other values. ErrorClass: {}, Argument: {} and"
+            " Traceback details are: {}".format(
+                error.__class__, error.args, error.__doc__
+            )
+        )
 
     # Return the model results to the web page
-    return render_template('index.html' ,message=message)
+    return render_template("index.html", message=message)
 
-if __name__ == '__main__':
-    app.run(debug=True , host='localhost', port=8080)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- model.py
+++ model.py
@@ -1,106 +1,241 @@
-import pandas as pd
-import numpy as np
-import json
-from pprint import pprint
+"""
+Trains a model on the UCI ML Auto MPG dataset.
+The dataset can be downloaded from:
+https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/
+"""
+
 import shutil
-
-from sklearn.compose import ColumnTransformer, make_column_selector
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-
-
-from xgboost import XGBRegressor
-
 import mlflow
 import mlflow.xgboost
+import pandas as pd
+import structlog
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from xgboost import XGBRegressor
+import warnings
+from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('./data/CarPrice_Assignment.csv')
-df.head()
+warnings.filterwarnings("ignore")
 
-df.info()
+logger = structlog.get_logger()
 
-df = df.drop(['car_ID','symboling', 'stroke','compressionratio','peakrpm'], axis=1)
 
-def preprocess_car_name(df):
-    df['CarBrand'] = df['CarName'].str.split(' ', n=1, expand=True)[0]
-    df = df.drop('CarName', axis=1)
-    df['CarBrand'] = df['CarBrand'].replace('alfa-romero', 'alfa-romeo')
-    df['CarBrand'] = df['CarBrand'].replace('maxda', 'mazda')
-    df['CarBrand'] = df['CarBrand'].replace('Nissan', 'nissan')
-    df['CarBrand'] = df['CarBrand'].replace('porcshce', 'porsche')
-    df['CarBrand'] = df['CarBrand'].replace('toyouta', 'toyota')
-    df['CarBrand'] = df['CarBrand'].replace(['vokswagen', 'vw'], 'volkswagen')
-    return df
-df = preprocess_car_name(df)
+def read_data(data_path: str) -> pd.DataFrame:
+    """
+    Read data from a csv file
 
-# Feature engineering
-numeric_transformer = Pipeline(
-    steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
-)
-categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+    Args:
+        data_path (str): path to the data file
 
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("cat", categorical_transformer, make_column_selector(dtype_include="object")),
-        ("num", numeric_transformer, make_column_selector(dtype_exclude="object")),
-    ]
-)
+    Returns:
+        pd.DataFrame: data
+    """
+    try:
+        data = pd.read_csv(data_path)
+    except FileNotFoundError:
+        logger.error("File not found")
+        return None
+    return data
 
-clf = Pipeline(
-    steps=[("preprocessor", preprocessor),
-           ("classifier", XGBRegressor())]
-)
 
-# Split Data
-price = df['price']
-dataset = df.drop('price', axis=1)
-X_train, X_test, y_train, y_test = train_test_split(dataset, price, test_size=0.2, random_state=1234)
+def preprocess(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Preprocess the data
 
-mlflow.sklearn.autolog()
+    Args:
+        data (pd.DataFrame): input data
 
-def fetch_logged_data(run_id):
+    Returns:
+        pd.DataFrame: preprocessed data
+    """
+    logger.info("Preprocessing data ...")
+    cleaned_df = data.copy()
+    cleaned_df = cleaned_df.drop(
+        ["car_ID", "symboling", "stroke", "compressionratio", "peakrpm"], axis=1
+    )
+    cleaned_df["CarBrand"] = cleaned_df["CarName"].str.split(" ", n=1, expand=True)[
+        0]
+    cleaned_df = cleaned_df.drop("CarName", axis=1)
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace(
+        "alfa-romero", "alfa-romeo")
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace("maxda", "mazda")
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace("Nissan", "nissan")
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace(
+        "porcshce", "porsche")
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace(
+        "toyouta", "toyota")
+    cleaned_df["CarBrand"] = cleaned_df["CarBrand"].replace(
+        ["vokswagen", "vw"], "volkswagen"
+    )
+    return cleaned_df
+
+
+def create_classifier() -> Pipeline:
+    """
+    Create a classifier pipeline
+
+    Returns:
+        sklearn.pipeline.Pipeline: classifier pipeline
+    """
+    logger.info("Creating classifier ...")
+    # Feature engineering
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", StandardScaler()),
+        ]
+    )
+    categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "cat",
+                categorical_transformer,
+                make_column_selector(dtype_include="object"),
+            ),
+            ("num", numeric_transformer, make_column_selector(dtype_exclude="object")),
+        ]
+    )
+    clf = Pipeline(
+        steps=[("preprocessor", preprocessor), ("classifier", XGBRegressor())]
+    )
+    return clf
+
+
+def split_data(data: pd.DataFrame, target: str = "price") -> tuple:
+    """
+    Split data into train and test sets
+
+    Args:
+        data (pd.DataFrame): input data
+        target (str): target column name
+
+    Returns:
+        tuple: train and test sets
+    """
+    logger.info("Splitting data ...")
+    X = data.drop(target, axis=1)
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def fetch_logged_data(run_id: str) -> tuple:
+    """
+    Fetch the data tracked by MLflow during the run with the specified ID.
+
+    Args:
+        run_id (str): ID of the run to fetch
+
+    Returns:
+        tuple: params, metrics, tags, artifacts
+    """
+    logger.info("Fetching logged data ...")
     client = mlflow.tracking.MlflowClient()
     data = client.get_run(run_id).data
     tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
     artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
     return data.params, data.metrics, tags, artifacts
 
-# gridsearsh
-param_grid = {
-    #"preprocessor__num__imputer__strategy": ["mean", "median"],
-    "classifier__max_depth": [6, 7, 8],
-    "classifier__learning_rate": [0.01, 0.015],
-    "classifier__min_child_weight": [1,2,3],
-    "classifier__subsample": [0.8, 0.9],
-    "classifier__colsample_bytree": [0.7, 0.8],
-    "classifier__n_estimators" : [500], # 600, 700],
-    #"classifier__reg_alpha" : [0, 0.05],
-    #"classifier__reg_lambda" : [0, 0.05],
-    "classifier__objective" : ['reg:squarederror']
-}
-with mlflow.start_run(run_name="run") as run:
 
-    grid_search = GridSearchCV(clf, 
-                                param_grid,
-                                cv=5,
-                                verbose=4,
-                                n_jobs=-1)
-    grid_search
+def hyperparameters_optimization(clf, X_train, y_train):
+    """
+    Hyperparameters optimization
 
-    grid_search.fit(X_train, y_train)
+    Args:
+        clf (sklearn.pipeline.Pipeline): classifier pipeline
+        X_train (pd.DataFrame): train data
+        y_train (pd.DataFrame): train target
 
-    params, metrics, tags, artifacts = fetch_logged_data(run.info.run_id)
-    pprint(params)
-    pprint(metrics)
-    pprint(tags)
-    pprint(artifacts)
-    pprint(run.info.run_id)
+    Returns:
+        sklearn.pipeline.Pipeline: optimized classifier pipeline
+    """
+    logger.info("Hyperparameters optimization ...")
+    # gridsearsh
+    param_grid = {
+        # "preprocessor__num__imputer__strategy": ["mean", "median"],
+        "classifier__max_depth": [6, 7, 8],
+        # "classifier__learning_rate": [0.01, 0.015],
+        # "classifier__min_child_weight": [1, 2, 3],
+        # "classifier__subsample": [0.8, 0.9],
+        # "classifier__colsample_bytree": [0.7, 0.8],
+        # "classifier__n_estimators": [500],  # 600, 700],
+        # # "classifier__reg_alpha" : [0, 0.05],
+        # # "classifier__reg_lambda" : [0, 0.05],
+        "classifier__objective": ["reg:squarederror"],
+    }
+    with mlflow.start_run(run_name="run") as run:
 
-    model_path = 'mlruns/0/{}/artifacts/model'.format(run.info.run_id)
+        grid_search = GridSearchCV(clf, param_grid, cv=5, verbose=4, n_jobs=-1)
 
-    shutil.copytree(model_path, './model', dirs_exist_ok=True)
+        grid_search.fit(X_train, y_train)
 
-    
+        params, metrics, tags, artifacts = fetch_logged_data(run.info.run_id)
+        logger.info("params: %s", params)
+        logger.info("metrics: %s", metrics)
+        logger.info("tags: %s", tags)
+        logger.info("artifacts: %s", artifacts)
+        logger.info("Best score: %f", grid_search.best_score_)
+        logger.info("Best parameters: %s", grid_search.best_params_)
+        logger.info("Best estimator: %s", grid_search.best_estimator_)
+        logger.info("Run ID: %s", run.info.run_id)
+
+        model_path = "mlruns/0/{}/artifacts/model".format(run.info.run_id)
+        logger.info("Saving model to %s", model_path)
+
+        shutil.copytree(model_path, "./model", dirs_exist_ok=True)
+    return grid_search.best_estimator_
+
+
+def train_model(best_estimator, X_train, y_train, X_test, y_test):
+    """
+    Train a model with the best hyperparameters
+
+    Args:
+        best_estimator (sklearn.pipeline.Pipeline): classifier pipeline
+        X_train (pd.DataFrame): train data
+        y_train (pd.DataFrame): train target
+        X_test (pd.DataFrame): test data
+        y_test (pd.DataFrame): test target
+
+    Returns:
+        sklearn.pipeline.Pipeline: trained classifier pipeline
+    """
+    logger.info("Training model ...")
+    with mlflow.start_run(run_name="run") as run:
+        best_estimator.fit(X_train, y_train)
+        y_pred = best_estimator.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        logger.info("mse: %s", mse)
+        params, metrics, tags, artifacts = fetch_logged_data(run.info.run_id)
+        logger.info("params: %s", params)
+        logger.info("metrics: %s", metrics)
+        logger.info("tags: %s", tags)
+        logger.info("artifacts: %s", artifacts)
+        logger.info("Run ID: %s", run.info.run_id)
+        model_path = "mlruns/0/{}/artifacts/model".format(run.info.run_id)
+        logger.info("Saving model to %s", model_path)
+        shutil.copytree(model_path, "./model", dirs_exist_ok=True)
+
+
+def main():
+    """
+    Main function
+    """
+    mlflow.sklearn.autolog()
+    data_path = "data/CarPrice_Assignment.csv"
+    data = read_data(data_path)
+    cleaned_data = preprocess(data)
+    X_train, X_test, y_train, y_test = split_data(cleaned_data)
+    clf = create_classifier()
+    best_estimator = hyperparameters_optimization(clf, X_train, y_train)
+    train_model(best_estimator, X_train, y_train, X_test, y_test)
+
+
+if __name__ == "__main__":
+    main()
